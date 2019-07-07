55ac19f69f1b338022a2d9515e6dd458100ff958
Simon Garner <simon.garner@madscience.co.nz>
2019-07-07 17:13:05 +1200

    Add convertTo() method for converting image to another format (#2)
    * Add convertTo() method for converting image to another format
    
    * Install @types/sharp
    
    * lint: fix @typescript-eslint/member-delimiter-style errors

--- src/ImageManipulator.spec.ts
+++ src/ImageManipulator.spec.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { ImageManipulator } from './ImageManipulator';
+import { ImageMimeType } from './ImageMimeType';
 
 test('can read metadata from an image', async () => {
   const manipulator = new ImageManipulator(fs.readFileSync(path.join(__dirname, 'test-image.png')));
@@ -30,4 +31,20 @@ test('can apply blur effect to an image', async () => {
 
   // not really any way for us to test it actually blurred, but at least we can be sure it ran without errors
   expect(metadata.mimeType).toBe('image/png');
+});
+
+test('can convert image from PNG to JPEG', async () => {
+  const manipulator = new ImageManipulator(fs.readFileSync(path.join(__dirname, 'test-image.png')));
+  const outputImage = await manipulator.convertTo(ImageMimeType.JPEG).toBuffer();
+  const metadata = await (new ImageManipulator(outputImage)).readMetadata();
+
+  expect(metadata.mimeType).toBe('image/jpeg');
+});
+
+test('can convert image from PNG to WEBP', async () => {
+  const manipulator = new ImageManipulator(fs.readFileSync(path.join(__dirname, 'test-image.png')));
+  const outputImage = await manipulator.convertTo(ImageMimeType.WEBP).toBuffer();
+  const metadata = await (new ImageManipulator(outputImage)).readMetadata();
+
+  expect(metadata.mimeType).toBe('image/webp');
 });
